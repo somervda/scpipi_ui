6815bcef9343bd9b91df3f7b0da3c868de00553a
David <39265971+somervda@users.noreply.github.com>
2024-03-21 07:27:59 -0400

    Added xdm1241 component and cleaned up look and feal

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MatTabsModule } from '@angular/material/tabs';
+import { Xdm1241Component } from './xdm1241/xdm1241.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Xdm1241Component, MatTabsModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
-  title = 'scpipi_ui';
+  title = 'SCPIPI';
 }
---
+++ src/app/xdm1241/xdm1241.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-xdm1241',
+  standalone: true,
+  imports: [],
+  templateUrl: './xdm1241.component.html',
+  styleUrl: './xdm1241.component.scss'
+})
+export class Xdm1241Component {
+
+}
