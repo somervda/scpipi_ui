31b8c342b4659f996a6a89913d55691bef12063f
David SurfacePro <somervda@comcast.net>
2024-03-24 11:25:24 -0400

    xdm1241 looking good

--- src/app/xdm1241/xdm1241.component.ts
+++ src/app/xdm1241/xdm1241.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
 import { LedpanelComponent } from '../ledpanel/ledpanel.component';
+import {MatButtonModule} from '@angular/material/button';
+
+
 
 @Component({
   selector: 'app-xdm1241',
   standalone: true,
-  imports: [LedpanelComponent],
+  imports: [LedpanelComponent,MatButtonModule],
   templateUrl: './xdm1241.component.html',
   styleUrl: './xdm1241.component.scss',
 })
