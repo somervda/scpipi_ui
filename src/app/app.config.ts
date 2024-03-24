997818b40a635f12d5f53b0a89e789c530caab16
David SurfacePro <somervda@comcast.net>
2024-03-24 11:52:36 -0400

    wip - getting xpm1241 working

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,11 +3,13 @@ import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { provideHttpClient } from '@angular/common/http';
 
 export enum Globals {
   HOSTANDPORT = 'scpipi.home:8000',
 }
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideAnimationsAsync()],
+  providers: [provideRouter(routes), provideAnimationsAsync(),provideHttpClient()],
+  
 };
--- src/app/xdm1241/xdm1241.component.ts
+++ src/app/xdm1241/xdm1241.component.ts
@@ -1,14 +1,52 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy } from '@angular/core';
 import { LedpanelComponent } from '../ledpanel/ledpanel.component';
-import {MatButtonModule} from '@angular/material/button';
+import { MatButtonModule } from '@angular/material/button';
+import { Xdm1241Service } from '../services/xdm1241.service';
+import { Subscription } from 'rxjs';
 
 
 
 @Component({
   selector: 'app-xdm1241',
   standalone: true,
-  imports: [LedpanelComponent,MatButtonModule],
+  imports: [LedpanelComponent, MatButtonModule],
   templateUrl: './xdm1241.component.html',
   styleUrl: './xdm1241.component.scss',
 })
-export class Xdm1241Component {}
+export class Xdm1241Component implements OnDestroy {
+  xdm1241Config$$: Subscription | undefined;
+  _type:string = '';
+
+  constructor(
+    private xdm1241Service: Xdm1241Service
+  ) { };
+
+
+configure(type:string): void {
+    console.log('configure', type);
+    this._type = type;
+    this.xdm1241Config$$ = this.xdm1241Service.configure(type,0,0).subscribe((result) => {
+      console.log(result)
+      if (result) {
+        this._type= type;
+      }
+      else {
+        alert("Could not connect to XDM1241!");
+        this._type='';
+      }
+     });
+
+}
+
+
+
+
+
+  ngOnDestroy(): void {
+   if (this.xdm1241Config$$) {
+      this.xdm1241Config$$.unsubscribe();
+    }
+  }
+
+
+}
