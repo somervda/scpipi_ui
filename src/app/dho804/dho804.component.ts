ae43a9e892760cd02d9488ead846915f7fadf79f
David <39265971+somervda@users.noreply.github.com>
2024-04-02 20:42:41 -0400

    Added dho804 components

---
+++ src/app/dho804/dho804.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-dho804',
+  standalone: true,
+  imports: [],
+  templateUrl: './dho804.component.html',
+  styleUrl: './dho804.component.scss'
+})
+export class Dho804Component {
+
+}
---
+++ src/app/services/dho804.service.ts
@@ -0,0 +1,9 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class Dho804Service {
+
+  constructor() { }
+}
