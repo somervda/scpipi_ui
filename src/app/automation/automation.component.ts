cfc2e12c366dfdbd7648b7aaf44ac3206c6f52de
David <39265971+somervda@users.noreply.github.com>
2024-04-04 17:30:16 -0400

    Automation looking better

--- src/app/automation/automation.component.ts
+++ src/app/automation/automation.component.ts
@@ -2,6 +2,12 @@ import { Component, ViewChild } from '@angular/core';
 import { MatTableModule, MatTable } from '@angular/material/table';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatCardModule } from '@angular/material/card';
+import { MatSelectModule } from '@angular/material/select';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { FormsModule } from '@angular/forms';
 import {
   AutomationService,
   Meter,
@@ -9,17 +15,20 @@ import {
 } from '../services/automation.service';
 import { MatSnackBar } from '@angular/material/snack-bar';
 
-export interface PeriodicElement {
-  name: string;
-  position: number;
-  weight: number;
-  symbol: string;
-}
-
 @Component({
   selector: 'app-automation',
   standalone: true,
-  imports: [MatTableModule, MatIconModule, MatButtonModule],
+  imports: [
+    MatTableModule,
+    MatIconModule,
+    MatButtonModule,
+    MatFormFieldModule,
+    MatInputModule,
+    FormsModule,
+    MatCardModule,
+    MatSelectModule,
+    MatCheckboxModule,
+  ],
   templateUrl: './automation.component.html',
   styleUrl: './automation.component.scss',
 })
@@ -27,20 +36,31 @@ export class AutomationComponent {
   displayedColumns: string[] = ['deviceName', 'type', 'delete'];
   dataSource;
 
-  @ViewChild(MatTable) table: MatTable<PeriodicElement> | undefined;
+  stepSeconds = 5;
+  maxSeconds = 2000;
+  jds6600StartHz = 1000;
+  jds6600StepFactor = 1.0595;
+  JDS6600Operator = '+';
+  jds6600StopHz = 20000;
+  jds6600: boolean = false;
+
+  @ViewChild(MatTable) table: MatTable<Meter> | undefined;
 
   constructor(
     private automationService: AutomationService,
     private _snackBar: MatSnackBar
   ) {
-    console.log(automationService.getMetersArray());
-    this.dataSource = automationService.getMetersArray();
+    this.dataSource = this.automationService.getMetersArray();
   }
 
   removeMeter(deviceName: string, type: string) {
     console.log('removeMeter:', deviceName, type);
     let meter: Meter = { deviceName: deviceName, type: type };
     this.automationService.removeMeter(meter);
+    this.refreshTable();
+  }
+
+  refreshTable() {
     this.dataSource = this.automationService.getMetersArray();
     if (this.table) {
       this.table.renderRows();
